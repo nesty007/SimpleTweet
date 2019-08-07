6dcf190d851fecad56cb112004c3aef18f96f5c2
Bernst G. Nestor <50707863+nesty007@users.noreply.github.com>
2019-08-07 13:24:47 -0400

    Adding Tweets

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -3,13 +3,19 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
+@Parcel
 public class User {
 
     public String name;
     public long uid;
     public String screeName;
     public String profileImageUrl;
+
+    public User(){
+
+    }
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
